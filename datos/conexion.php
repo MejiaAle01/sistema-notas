b0c57ee1b5853e1e362b816c596cf14af481f1c8
Alejandro Mejía <83927564+MejiaAle01@users.noreply.github.com>
2023-02-05 12:22:40 -0600

    Update conexion.php

--- datos/conexion.php
+++ datos/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	try {
-		$cn = new PDO("mysql:host=localhost;dbname=bdescuela","escuela","escuela");
+		$cn = new PDO("mysql:host=xxxxxxx;dbname=xxxxxxxx","xxxxxxx","xxxxxxxx");
 	} catch (PDOException $ex) {
 		print_r("Error: ". $ex->getMessage());
 		//die($ex->getMessage());
